73a2e1caf43624ec77d0a57ffc7869985a0bf7f0
tervingo <keltivas@gmail.com>
2025-03-03 13:22:55 +0100

    "Entrantes" quitados de la lista de categorías

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,6 @@ export const BACKEND_URL = 'https://recetarium-back.onrender.com';
 export const CATEGORIES = [
     "Aperitivos",
     "Tapas y Pinchos",
-    "Entrantes",
     "Primeros",
     "Segundos",
     "Guarniciones",
